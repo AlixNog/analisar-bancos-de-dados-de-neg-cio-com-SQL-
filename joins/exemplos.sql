53de0aa7e94a7509adcc5e91dc21e24608d0df18
AlixNog <alicebnog92@gmail.com>
2024-06-18 12:02:37 -0300

    Update pesquisas

--- joins/exemplos.sql
+++ joins/exemplos.sql
@@ -1,15 +1,46 @@
--- EXERCÍCIOS ########################################################################
+-- (Exercício 1) Identifique quais as marcas de veículo mais visitada na tabela sales.funnel
 
--- (Exemplo 1) Identifique qual é o status profissional mais frequente nos clientes 
--- que compraram automóveis no site
-
-
--- (Exemplo 2) Identifique qual é o gênero mais frequente nos clientes que compraram
--- automóveis no site. Obs: Utilizar a tabela temp_tables.ibge_genders
-select * from temp_tables.ibge_genders limit 10
+select 
+	pro.brand,
+	count(*) as visitas
+	 
 
 
--- (Exemplo 3) Identifique de quais regiões são os clientes que mais visitam o site
--- Obs: Utilizar a tabela temp_tables.regions
-select * from sales.customers limit 10
-select * from temp_tables.regions limit 10
+from sales.funnel as fun
+left join sales.products as pro
+	on fun.product_id = pro.product_id
+
+group by pro.brand
+order by visitas desc
+
+
+-- (Exercício 2) Identifique quais as lojas de veículo mais visitadas na tabela sales.funnel
+
+select 
+	sto.store_name,
+	count(*) as "lojas mais visitadas"
+
+
+from sales.funnel as fun
+left join sales.stores as sto
+	on fun.store_id = sto.store_id
+
+group by sto.store_name
+order by "lojas mais visitadas" desc
+
+-- (Exercício 3) Identifique quantos clientes moram em cada tamanho de cidade (o porte da cidade
+-- consta na coluna "size" da tabela temp_tables.regions)
+
+select 
+	reg.size,
+	
+	count(*) as contagem
+
+	
+from sales.customers as cus
+left join temp_tables.regions as reg
+	on lower(cus.city) = lower(reg.city)
+	and lower (cus.state) = lower(reg.state)
+
+group by reg.size
+order by contagem desc
